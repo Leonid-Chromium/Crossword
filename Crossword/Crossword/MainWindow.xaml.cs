968e64338a5efe7202d11f3dc754c5ad28840848
Leonid-Chromium <l.khromenko@gmail.com>
2021-12-25 21:00:52 +0700

    FirstRelease

--- Crossword/Crossword/MainWindow.xaml.cs
+++ Crossword/Crossword/MainWindow.xaml.cs
@@ -23,8 +23,8 @@ namespace Crossword
     /// 
     public class Coordinate
     {
-        public uint abcise { get; set; }
-        public uint ordinate { get; set; }
+        public uint abcise { get; set; } = 1;
+        public uint ordinate { get; set; } = 1;
 
 
         public Coordinate(uint x, uint y)
@@ -41,15 +41,52 @@ namespace Crossword
         public MainWindow()
         {
             InitializeComponent();
+            init("Определение", "Значение", new Coordinate(2, 2), true);
             init("Влияние неопределённости на процесс достижение поставленных целей", "Риск", new Coordinate(1, 1), true);
-            init("Неправомерное получение информации с использованием технического средства, осуществляющего обнаружение, приём и обработку информативных сигналов", "Перехват", new Coordinate(1, 1), true);
-            init("Определение", "Значениеkj", new Coordinate(1, 10), true);
-            init("Определение", "Значение2", new Coordinate(1, 4), false);
-            init("Определение", "Значение3", new Coordinate(4, 1), true);
-            init("Определение", "Значение4", new Coordinate(1, 1), true);
-            init("Определение", "Значение5", new Coordinate(1, 1), true);
+            init("Неправомерное получение информации с использованием технического средства, осуществляющего обнаружение, приём и обработку информативных сигналов", "Перехват", new Coordinate(20, 20), false);
+            init("Определение", "Значение1", new Coordinate(3, 3), true);
+            init("Определение", "Значение2", new Coordinate(4, 4), true);
+            init("Определение", "Значение3", new Coordinate(5, 5), true);
+            init("Определение", "Значение4", new Coordinate(6, 6), true);
+            init("Определение", "Значение5", new Coordinate(7, 7), false);
 
+            //testDataTableFunction(dataTable);
 
+            Trace.WriteLine("");
+            Trace.WriteLine("Общая информация");
+            Trace.WriteLine(String.Format("x = " + dataTable.Columns.Count));
+            Trace.WriteLine(String.Format("y = " + dataTable.Rows.Count));
+
+            DTtoTrace(dataTable);
+            myDataGrid.ItemsSource = dataTable.DefaultView;
+        }
+
+        public void DTtoTrace(DataTable dataTable)
+        {
+            Trace.WriteLine("");
+            Trace.WriteLine("Общая информация");
+            Trace.WriteLine(String.Format("x = " + dataTable.Columns.Count));
+            Trace.WriteLine(String.Format("y = " + dataTable.Rows.Count));
+
+            for (int i = 0; i < dataTable.Rows.Count; i++)
+            {
+                Trace.Write("|");
+                for (int j = 0; j < dataTable.Columns.Count; j++)
+                {
+                    Trace.Write(String.Format("{0,3}",dataTable.Rows[i].ItemArray[j].ToString()));
+                    Trace.Write("|");
+                }
+                Trace.WriteLine("");
+            }
+
+            for(int i = 0; i<dataTable.Columns.Count; i++)
+            {
+                Trace.WriteLine(String.Format(dataTable.Columns[i].ColumnName + " " + dataTable.Columns[i].DataType));
+            }
+        }
+
+        public void testDataTableFunction(DataTable dataTable)
+        {
             dataTable.Columns.Add("Дата");
             dataTable.Columns.Add("Время");
             dataTable.Columns.Add("Тип события");
@@ -58,34 +95,18 @@ namespace Crossword
             {
                 dataTable.Rows.Add("t", "e", "s", "t");
             }
-
-            Trace.WriteLine("");
-            Trace.WriteLine("общая информация");
-            Trace.WriteLine(String.Format("x = " + dataTable.Columns.Count));
-            Trace.WriteLine(String.Format("y = " + dataTable.Rows.Count));
-            
-            for(int i = 0; i<dataTable.Rows.Count; i++)
-            {
-                for(int j = 0; j<dataTable.Columns.Count; j++)
-                {
-                    Trace.Write(dataTable.Rows[i].ItemArray[j].ToString());
-                    Trace.Write("|");
-                }
-                Trace.WriteLine("");
-            }
-
-            myDataGrid.DataContext = myDataGrid;
+            dataTable.Rows[18].ItemArray[5] = 2;
         }
 
         public void init(string description, string value, Coordinate startCoordinate, bool vector)
         {
             // true = vertical
 
-            //Trace.WriteLine(String.Format("description = " + description));
-            //Trace.WriteLine(String.Format("value = " + value));
-            //Trace.WriteLine(String.Format("start ord = " + startCoordinate.ordinate));
-            //Trace.WriteLine(String.Format("start abc = " + startCoordinate.abcise));
-            //Trace.WriteLine(String.Format("Vector = " + vector));
+            Trace.WriteLine(String.Format("description = " + description));
+            Trace.WriteLine(String.Format("value = " + value));
+            Trace.WriteLine(String.Format("start ord = " + startCoordinate.ordinate));
+            Trace.WriteLine(String.Format("start abc = " + startCoordinate.abcise));
+            Trace.WriteLine(String.Format("Vector = " + vector));
 
             StackPanel stackPanel = new StackPanel();
             stackPanel.Orientation = Orientation.Horizontal;
@@ -108,13 +129,14 @@ namespace Crossword
                 HorizontalST.Children.Add(stackPanel);
             }
 
-            DTFun(startCoordinate.abcise, startCoordinate.ordinate, value, vector);
+            formatDTFunction(startCoordinate.abcise, startCoordinate.ordinate, value, vector);
+            fillDT(startCoordinate.abcise, startCoordinate.ordinate, value, vector);
 
             Trace.WriteLine(String.Format("x = " + dataTable.Columns.Count));
             Trace.WriteLine(String.Format("y = " + dataTable.Rows.Count));
         }
 
-        public void DTFun(uint x, uint y, string value, bool vector)
+        public void formatDTFunction(uint x, uint y, string value, bool vector)
         {
             if (vector)
                 y = (y + (uint)value.Length) - 1;
@@ -125,7 +147,48 @@ namespace Crossword
                 dataTable.Columns.Add(Convert.ToString(dataTable.Columns.Count), typeof(string));
 
             while (y > dataTable.Rows.Count)
-                dataTable.Rows.Add(Convert.ToString(dataTable.Rows.Count));
+                dataTable.Rows.Add(/*Convert.ToString(dataTable.Rows.Count)*/);
+        }
+
+        public void fillDT(uint x, uint y, string value, bool vector)
+        {
+            int newX = Convert.ToInt32(x) - 1;
+            int newY = Convert.ToInt32(y) - 1;
+            Trace.WriteLine(String.Format("Длина слова = " + value.Length));
+            if (vector)
+            {
+                for (int j = 0; j < value.Length; j++)
+                {
+                    try
+                    {
+                        Trace.Write(value[j]);
+                        dataTable.Rows[j + newY].SetField(dataTable.Columns[newX], value[j]);
+                        Trace.WriteLine(dataTable.Rows[j + newY].ItemArray[x]);
+                    }
+                    catch(Exception ex)
+                    {
+                        Trace.WriteLine("");
+                        Trace.WriteLine(ex.Message);
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 0; i<value.Length; i++)
+                {
+                    try
+                    {
+                        Trace.Write(value[i]);
+                        dataTable.Rows[newY].SetField(dataTable.Columns[i + newX], value[i]);
+                        Trace.WriteLine(dataTable.Rows[newY].ItemArray[x + i]);
+                    }
+                    catch (Exception ex)
+                    {
+                        Trace.WriteLine("");
+                        Trace.WriteLine(ex.Message);
+                    }
+                }
+            }
         }
     }
 }
